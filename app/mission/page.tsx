76f1a1562cae0ddd5e9b700e8d8b6fd10bbb2392
CaptainAlpha04 <aliimran.iam59@gmail.com>
2024-09-17 16:07:12 +0500

    fixed bugs and issues

--- app/mission/page.tsx
+++ app/mission/page.tsx
@@ -150,7 +150,7 @@ function Page() {
             </div>
           </section>
 
-          <section className="relative w-screen h-fit py-20 px-10 pb-30 flex flex-col bg-gradient-to-b from-slate-950 to-base-300">
+          <section className="relative w-screen h-fit py-20 px-10 mb-20  flex flex-col bg-gradient-to-b from-slate-950 to-base-300">
             <h2 className="text-6xl font-bold text-center text-base-content">Global Impact</h2>
             <p className='text-md mt-16 font-light drop-shadow-lg md:px-32 text-justify'>We aim to create a <strong className='text-lg'>global impact</strong> by fostering a community of innovators, entrepreneurs, and leaders who are equipped to <strong className='text-lg'>solve</strong> challenges that transcend borders. Through collaboration across industries and cultures, SPARK <strong className='text-lg'>connects</strong> individuals worldwide, enabling the exchange of ideas, skills, and resources. By focusing on <strong className='text-lg'>universal</strong> pillars like creativity, research, and technology, Our initiatives are designed to address global issues such as education reform, <strong className='text-lg'>sustainable</strong> innovation, and social entrepreneurship, ensuring that its solutions benefit not only local communities but also the global <strong className='text-lg'>society</strong> at large.</p>
             <img src="/designs/globe.png" alt="" className='absolute self-center opacity-30 mask-image-gradient minh-96 min-w-96 w-96'/>
